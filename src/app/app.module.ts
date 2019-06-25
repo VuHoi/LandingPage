d5d62d2bed1f1d854b9372227f4f75d7451a4c81
vuhoi <toilahoi007@gmail.com>
2019-06-25 21:57:24 +0700

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainSlideComponent } from './main-slide/main-slide.component';
+import { IntroduceComponent } from './introduce/introduce.component';
+import { CustomCardComponent } from './custom-card/custom-card.component';
+import { ContactComponent } from './contact/contact.component';
+import { SlideComponent } from './slide/slide.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainSlideComponent,
+    IntroduceComponent,
+    CustomCardComponent,
+    ContactComponent,
+    SlideComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
